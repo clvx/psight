c3c3fea67196d342350610fb5df1b97049958c59
Michael Ibarra <michael.ibarra@gmail.com>
2020-03-23 22:14:20 -0400

    handling basic requests - built-in server

--- web/app/src/github.com/lss/webapp/main.go
+++ web/app/src/github.com/lss/webapp/main.go
@@ -1,39 +1,23 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//look into the request object, check the file, and translate it to the fs
-		f, err := os.Open("public" + r.URL.Path)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
-		}
-		defer f.Close()
-
-		//Check request content type
-		var contentType string
-		switch {
-		case strings.HasSuffix(r.URL.Path, "css"):
-			contentType = "text/css"
-		case strings.HasSuffix(r.URL.Path, "html"):
-			contentType = "text/html"
-		case strings.HasSuffix(r.URL.Path, "png"):
-			contentType = "text/png"
-		default:
-			contentType = "text/plain"
-		}
-		//Add a Content-Type header in ResponseWriter object
-		w.Header().Add("Content-Type", contentType)
-		//Copy file content directly to http.ResponseWriter which implements Writer interface
-		io.Copy(w, f)
+	/*
+	//This piece of code gives the same functionality as the one liner below
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		// ServeFile replies to the request with the contents of the named
+		// file or directory.
+		http.ServeFile(w, r, "public"+r.URL.Path)
+		
 	})
-	http.ListenAndServe(":8000", nil)
+	*/
+
+	//FileServer returns a handler that serves HTTP requests
+	// with the contents of the file system rooted at root.
+	//A Dir implements FileSystem using the native file system restricted to a
+	// specific directory tree.
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
