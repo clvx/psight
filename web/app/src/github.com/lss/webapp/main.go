c92b16ab22e3871b8092e10369916cc365830b06
Michael Ibarra <michael.ibarra@gmail.com>
2020-03-25 11:59:55 -0400

    loading templates

--- web/app/src/github.com/lss/webapp/main.go
+++ web/app/src/github.com/lss/webapp/main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
-	/*
-	//This piece of code gives the same functionality as the one liner below
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		// ServeFile replies to the request with the contents of the named
-		// file or directory.
-		http.ServeFile(w, r, "public"+r.URL.Path)
-		
-	})
-	*/
 
-	//FileServer returns a handler that serves HTTP requests
-	// with the contents of the file system rooted at root.
-	//A Dir implements FileSystem using the native file system restricted to a
-	// specific directory tree.
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	templates := populateTemplates() //Loading templates
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:] // removing leading / character
+		//Find template based on requested page name
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			//Rendering and writing template to http.ResponseWriter which implements io.Writer
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			//Writing 404 to http.ResponseWriter.WriteHeader
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates") //returns a pointer to a new templates struct
+	const basePath = "templates"        // templates directory
+	//parsing all *.html matches in templates/ or fails
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
